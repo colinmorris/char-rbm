4e43db73305f967693e4fec249fbcdd12fda0fc0
colinmorris <colin.morris2@gmail.com>
2016-05-28 11:02:38 -0700

    Allow sample.py to take multiple model files

--- sample.py
+++ sample.py
@@ -30,8 +30,8 @@ def sample_model(model, n, iters, prog):
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Sample short texts from a pickled model',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument('model_fname', metavar='model.pickle',
-                      help='A pickled RBM model')
+  parser.add_argument('model_fname', metavar='model.pickle', nargs='+',
+                      help='One or more pickled RBM models')
   parser.add_argument('-n', '--n-samples', dest='n_samples', type=int, default=20,
 	                    help='How many samples to draw')
   parser.add_argument('-i', '--iters', dest='iters', type=int, default=1000,
@@ -41,7 +41,9 @@ if __name__ == '__main__':
                       
   args = parser.parse_args()
 
-  f = open(args.model_fname)
-  model = pickle.load(f)
-  f.close()
-  sample_model(model, args.n_samples, args.iters, args.prog)
+  for model_fname in args.model_fname:
+    print "Drawing samples from model defined at {}".format(model_fname)
+    f = open(model_fname)
+    model = pickle.load(f)
+    f.close()
+    sample_model(model, args.n_samples, args.iters, args.prog)
