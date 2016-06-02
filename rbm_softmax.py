Fix PL in CharBernoulliRBM, and a note on sus free energy calculation

rbm_softmax.py
@@ -178,6 +178,7 @@ class BernoulliRBM(BaseEstimator, TransformerMixin):
         free_energy : array-like, shape (n_samples,)
             The value of the free energy.
         """
+        # TODO: This doesn't seem to match with eq 2.7 in http://www.jmlr.org/proceedings/papers/v9/marlin10a/marlin10a.pdf
         return (- safe_sparse_dot(v, self.intercept_visible_)
                 - np.logaddexp(0, safe_sparse_dot(v, self.components_.T)
                                + self.intercept_hidden_).sum(axis=1))
@@ -445,6 +446,30 @@ class CharBernoulliRBM(BernoulliRBM):
         sample[i,j,one_indices] = 1
         return sample.reshape(n, maxlen*nchars)
 
+    def corrupt(self, v):
+        n_softmax, n_opts = self.softmax_shape
+        # Select a random index in to the indices of the non-zero values of each input
+        # TODO: In the char-RBM case, if I wanted to really challenge the model, I would avoid selecting any
+        # trailing spaces here. Cause any dumb model can figure out that it should assign high energy to
+        # any instance of /  [^ ]/
+        meta_indices_to_corrupt = self.rng_.randint(0, n_softmax, v.shape[0]) + np.arange(0, n_softmax * v.shape[0], n_softmax)
+
+        # Offset these indices by a random amount (but not 0 - we want to actually change them)
+        offsets = self.rng_.randint(1, n_opts, v.shape[0])
+        # Also, do some math to make sure we don't "spill over" into a different softmax.
+        # E.g. if n_opts=5, and we're corrupting index 3, we should choose offsets from {-3, -2, -1, +1}
+        # 1-d array that matches with meta_i_t_c but which contains the indices themselves
+        indices_to_corrupt = v.indices[meta_indices_to_corrupt]
+        # Sweet lucifer
+        offsets = offsets - (n_opts * (((indices_to_corrupt % n_opts) + offsets.ravel()) >= n_opts))
+
+        v.indices[meta_indices_to_corrupt] += offsets
+        return v, (meta_indices_to_corrupt, offsets)
+
+    def uncorrupt(self, visibles, state):
+        mitc, offsets = state
+        visibles.indices[mitc] -= offsets
+
 
 class CharBernoulliRBMSoftmax(CharBernoulliRBM):
 
@@ -473,28 +498,4 @@ class CharBernoulliRBMSoftmax(CharBernoulliRBM):
             return self.sample_max(utils.softmax(reshaped))
         return utils.softmax_and_sample(reshaped).reshape((nsamples, nfeats))
 
-    def corrupt(self, v):
-        # TODO: Should probably make this available in the parent class as well.
-        # So that the two models can be compared on even ground.
-        n_softmax, n_opts = self.softmax_shape
-        # Select a random index in to the indices of the non-zero values of each input
-        # TODO: In the char-RBM case, if I wanted to really challenge the model, I would avoid selecting any
-        # trailing spaces here. Cause any dumb model can figure out that it should assign high energy to
-        # any instance of /  [^ ]/
-        meta_indices_to_corrupt = self.rng_.randint(0, n_softmax, v.shape[0]) + np.arange(0, n_softmax * v.shape[0], n_softmax)
-
-        # Offset these indices by a random amount (but not 0 - we want to actually change them)
-        offsets = self.rng_.randint(1, n_opts, v.shape[0])
-        # Also, do some math to make sure we don't "spill over" into a different softmax.
-        # E.g. if n_opts=5, and we're corrupting index 3, we should choose offsets from {-3, -2, -1, +1}
-        # 1-d array that matches with meta_i_t_c but which contains the indices themselves
-        indices_to_corrupt = v.indices[meta_indices_to_corrupt]
-        # Sweet lucifer
-        offsets = offsets - (n_opts * (((indices_to_corrupt % n_opts) + offsets.ravel()) >= n_opts))
-
-        v.indices[meta_indices_to_corrupt] += offsets
-        return v, (meta_indices_to_corrupt, offsets)
-
-    def uncorrupt(self, visibles, state):
-        mitc, offsets = state
-        visibles.indices[mitc] -= offsets
+    