79e4aedfc837a3fe0bc3685681e1bb43bc8580f4
colinmorris <colin.morris2@gmail.com>
2016-07-08 11:06:09 -0700

    if name = main block in energy_checker

--- energy_checker.py
+++ energy_checker.py
@@ -15,24 +15,25 @@ def energizer(model, s):
     assert nrg.shape == (1,)
     return nrg[0]
 
-parser = argparse.ArgumentParser(description="Check the energy a model assigns to some strings, interactively or by passing them on the command line")
-parser.add_argument('model_file')
-parser.add_argument('string', nargs='*')
-parser.add_argument('-i', '--interactive', action='store_true')
-args = parser.parse_args()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Check the energy a model assigns to some strings, interactively or by passing them on the command line")
+    parser.add_argument('model_file')
+    parser.add_argument('string', nargs='*')
+    parser.add_argument('-i', '--interactive', action='store_true')
+    args = parser.parse_args()
 
-with open(args.model_file) as f:
-    model = pickle.load(f)
+    with open(args.model_file) as f:
+        model = pickle.load(f)
 
-for s in args.string:
-   print "E({}) = {:.2f}".format(repr(s), energizer(model, s))
+    for s in args.string:
+       print "E({}) = {:.2f}".format(repr(s), energizer(model, s))
 
-if args.interactive or len(args.string) == 0:
-    print "Get energies of some strings interactively! q to quit"
-    while 1:
-        s = raw_input("Get energy of: ")
-        if s.lower() == 'q':
-            break
-        print "{:.2f}".format(energizer(model, s))
+    if args.interactive or len(args.string) == 0:
+        print "Get energies of some strings interactively! q to quit"
+        while 1:
+            s = raw_input("Get energy of: ")
+            if s.lower() == 'q':
+                break
+            print "{:.2f}".format(energizer(model, s))
 
 
